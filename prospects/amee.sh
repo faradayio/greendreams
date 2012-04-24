41788a4df85746ad39e820a0027c4a4f2adc4adc
Andy Rossmeissl <andy@rossmeissl.net>
2012-04-24 10:16:26 -0400

    No longer necessary

--- prospects/amee.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-/usr/bin/curl -i 'https://api-stage.amee.com/3.6/categories/DEFRA_transport_fuel_methodology/items;values?fuel=petrol' \
-  -H 'Accept: application/json' \
-  -u greendreams1:ynzpd69c \
-  -s \
-  -H 'Origin: http://example.com'
